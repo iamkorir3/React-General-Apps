668e021c72548945cd4f9dc86f6f6c077e1995c5
Iamkorir3 <korir5956@gmail.com>
2025-06-22 22:21:35 +0300

    new commit

---
+++ src/currency converter/App.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [amount, setCurrency] = useState(10);
+  const [output, setOutput] = useState(10);
+  const [firstCunrrency, setFirstCunrrency] = useState("USD");
+  const [secondCunrrency, setSecondCunrrency] = useState("EUR");
+
+  function logValues() {
+    console.log(amount);
+    console.log(firstCunrrency);
+    console.log(secondCunrrency);
+  }
+
+  useEffect(function () {
+    async function getCurrency() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${firstCunrrency}&to=${secondCunrrency}`
+      );
+
+      const data = await res.json();
+      setOutput(data);
+    }
+    getCurrency();
+  }, []);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setCurrency(e.target.value)}
+      />
+      <select
+        value={firstCunrrency}
+        onChange={(e) => setFirstCunrrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={secondCunrrency}
+        onChange={(e) => setSecondCunrrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <button onClick={logValues}>LOG</button>
+      <p>{output}</p>
+    </div>
+  );
+}
+
+//`https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
